6d5b9d33397331b0381322b1f39705bd45ef4207
Jacob Chen <jacob@heringchen.com>
2020-09-07 12:20:16 -0400

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Jacob Chen on 9/7/20.
+//  Copyright © 2020 jacobpchen@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
