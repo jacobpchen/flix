b214b91041c55317a0e44b4ea40beef2c8ec8595
Jacob Chen <jacob@heringchen.com>
2020-09-12 14:33:00 -0400

    uploaded "flix" app

---
+++ flix/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Jacob Chen on 9/12/20.
+//  Copyright © 2020 jacobpchen@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
