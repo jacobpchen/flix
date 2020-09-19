bbe2aaa18e192e0d87570c23d80af70a789eba53
Jacob Chen <jacob@heringchen.com>
2020-09-19 15:05:23 -0400

    Added collection view

---
+++ flix/MovieCollectionDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCollectionDetailsViewController.swift
+//  flix
+//
+//  Created by Jacob Chen on 9/19/20.
+//  Copyright © 2020 jacobpchen@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        print("Loading")
+    }
+    
+
+}
---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Jacob Chen on 9/19/20.
+//  Copyright © 2020 jacobpchen@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
